9a629facf5b5810a32b1d0f05b599248457bf5fd
THYAGO PINHEIRO DINIZ <thy-diniz@hotmail.com>
2023-04-28 21:21:13 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static(__dirname+'/publico'));
 app.get("/", function(req, res){
     res.sendFile(__dirname+"/TelaJavaScript.html");
 });
