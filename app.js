4b67d0df6a29c7b066ccaa72198aa3d47c7c9820
THYAGO PINHEIRO DINIZ <thy-diniz@hotmail.com>
2023-04-28 21:05:39 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,24 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get("/", function(req, res){
-    res.send("Bem vindo usuario");
-})
-app.get("/professor", function(req, res){
-    res.send("<h1>Bem vindo professor</h1>");
-})
-// app.get("/professor/:parametro", function(req, res){
-//     //req - request - dados enviados pelo cliente
-//     //res - response - dados enviados do servidor para o cliente
-//     res.send("<h1>Professor consulta</h1>" + req.params.parametro);
-// })
-app.get("/professor/nuvem/:nome?", function(req, res){
-    var nome = req.params.nome;
-    if(nome){
-        res.send("<h1>Professor consulta</h1>" + nome + " para liberar.");
-    }else{
-        res.send("<h1>Professor libera!</h1>");
-    }
-})
+    res.sendFile(__dirname+"TelaJavaScript.html");
+});
 
 app.listen(3000,function(erro){
     
